c01fa66750a4428b16fc7ecc3d76c754dfec7867
Vittoria <vbenedetoamerico@gmail.com>
2025-10-02 13:12:57 +0200

    Add delete book card logic

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const books = document.querySelector(".books");
 const addBtn = document.querySelector(".add-btn");
+const deleteBtn = document.querySelector(".deleteBtn");
 const dialog = document.querySelector("dialog");
 const form = document.getElementById("form");
-const myLibrary = [];
+let myLibrary = [];
 
 
 addBtn.addEventListener("click", () => {
+  form.querySelectorAll("input[type='text'], input[type='number']").forEach(input => input.value = "");
   dialog.showModal();
 })
 
@@ -16,27 +18,67 @@ form.addEventListener("submit", (event) => {
   const title = document.getElementById("bookTitle").value;
   const author = document.getElementById("bookAuthor").value;
   const pages = document.getElementById("bookPages").value;
-  const readStatus = document.querySelector('input[name="readStatus"]:checked').value;
- 
-  const book = new Book(title, author, pages, readStatus);
-  console.log(book);
+  const readStatus = document.querySelector("input[name='readStatus']:checked").value;
+  const isRead = readStatus === "true";
+  const book = new Book(title, author, pages, isRead);
   addBookToLibrary(book);
+  dialog.close();
 })
 
 function Book(title, author, pages, read) {
   if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor ")
+    throw Error("You must use the 'new' operator to call the constructor");
   }
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = Boolean(read);
+  this.read = read;
   this.id = crypto.randomUUID();
 }
 
-function createBookCard() {
+function createBookCard(book, bookId) {
   const newCard = document.createElement("div");
-    
+  newCard.classList.add("book");
+  newCard.id = bookId;
+
+  const title = document.createElement("h2");
+  title.textContent = `${book.title}`;
+  title.classList.add("card-title");
+  newCard.appendChild(title);
+
+  const author = document.createElement("h3");
+  author.textContent = `By ${book.author}`;
+  author.classList.add("card-author");
+  newCard.appendChild(author);
+
+  const pages = document.createElement("p");
+  pages.textContent = `${book.pages} pages`;
+  pages.classList.add("card-pages");
+  newCard.appendChild(pages);
+
+  const readStatus = document.createElement("p");
+  readStatus.textContent = book.read ? "Readed: Yes" : "Readed: No";
+  readStatus.classList.add("card-status");
+  newCard.appendChild(readStatus);
+
+  const switchStatusBtn = document.createElement("button");
+  switchStatusBtn.textContent = "Change Read Status";
+  switchStatusBtn.classList.add("switchStatusBtn");
+  newCard.appendChild(switchStatusBtn);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete Book";
+  deleteBtn.classList.add("deleteBtn");
+  newCard.appendChild(deleteBtn);
+
+  deleteBtn.addEventListener("click", (event) => {
+    const parentCard = event.target.parentElement;
+    const bookId = parentCard.id;
+    myLibrary = myLibrary.filter((book) => book.id !== bookId);
+    displayLibrary();
+  })
+
+  return newCard;
 }
 
 function addBookToLibrary(book) {
@@ -49,27 +91,8 @@ function displayLibrary() {
   books.innerHTML = "";
   myLibrary.forEach((book) => {
     const bookId = book.id;
-    const newCard = document.createElement("div");
-      newCard.classList.add("book");
-      newCard.id = bookId;
-
-    const title = document.createElement("h2");
-      title.textContent = `${book.title}`;
-      newCard.appendChild(title);
-
-    const author = document.createElement("h3");
-      author.textContent = `By ${book.author}`;
-      newCard.appendChild(author);
-
-    const pages = document.createElement("p");
-      pages.textContent = `${book.pages} pages`;
-      newCard.appendChild(pages);
-    
-    const readStatus = document.createElement("p");
-      readStatus.textContent = book.read === true ? "Readed: Yes" : "Readed: No";
-      newCard.appendChild(readStatus);
-
-    books.appendChild(newCard);
+    const newBookCard = createBookCard(book, bookId);
+    books.appendChild(newBookCard);
   })
 }
 
