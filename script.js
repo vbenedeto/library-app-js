4db3f6c2410db97fd5edc00f2c5013140274412b
Vittoria <vbenedetoamerico@gmail.com>
2025-12-02 20:37:00 +0100

    Use class constructor and organise code

--- script.js
+++ script.js
@@ -1,48 +1,43 @@
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+  }
+
+  toggleStatus() {
+    this.read = !this.read;
+  }
+}
+
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(...newBooks) {
+    this.books.push(...newBooks);
+  }
+
+  removeBook(id) {
+    this.books = this.books.filter(book => book.id !== id);
+  }
+
+  getBooks() {
+    return this.books;
+  }
+}
+
 const books = document.querySelector(".books");
 const addBtn = document.querySelector(".add-btn");
-const deleteBtn = document.querySelector(".deleteBtn");
 const dialog = document.querySelector("dialog");
 const closeDialogBtn = document.querySelector(".close-dialog-container__btn");
 const form = document.getElementById("form");
-let myLibrary = [];
 
-addBtn.addEventListener("click", () => {
-  form.querySelectorAll("input[type='text'], input[type='number']").forEach(input => input.value = "");
-  dialog.showModal();
-});
-
-closeDialogBtn.addEventListener("click", () => {
-  dialog.close();
-});
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  //Create a new book
-  const title = document.getElementById("bookTitle").value;
-  const author = document.getElementById("bookAuthor").value;
-  const pages = document.getElementById("bookPages").value;
-  const readStatus = document.querySelector("input[name='readStatus']:checked").value;
-  const isRead = readStatus === "true";
-  const book = new Book(title, author, pages, isRead);
-  addBookToLibrary(book);
-  dialog.close();
-})
-
-function Book(title, author, pages, read) {
-  if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
-  }
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.id = crypto.randomUUID();
-}
-
-Book.prototype.toggleStatus = function() {
-  this.read = !this.read;
-};
+const myLibrary = new Library();
 
 function createBookCard(book, bookId) {
   const newCard = document.createElement("div");
@@ -95,7 +90,8 @@ function createBookCard(book, bookId) {
   deleteBtn.addEventListener("click", (event) => {
     const parentCard = event.target.closest(".card");
     const bookId = parentCard.id;
-    myLibrary = myLibrary.filter((book) => book.id !== bookId);
+
+    myLibrary.removeBook(bookId);
     displayLibrary();
   })
 
@@ -108,19 +104,40 @@ function createBookCard(book, bookId) {
 }
 
 function addBookToLibrary(book) {
-  myLibrary.push(book);
-
+  myLibrary.addBook(book);
   displayLibrary();
 }
 
 function displayLibrary() {
   books.innerHTML = "";
-  myLibrary.forEach((book) => {
+  myLibrary.books.forEach((book) => {
     const bookId = book.id;
     const newBookCard = createBookCard(book, bookId);
     books.appendChild(newBookCard);
   })
 }
+
+addBtn.addEventListener("click", () => {
+ form.reset();
+  dialog.showModal();
+});
+
+closeDialogBtn.addEventListener("click", () => {
+  dialog.close();
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = document.getElementById("bookTitle").value;
+  const author = document.getElementById("bookAuthor").value;
+  const pages = document.getElementById("bookPages").value;
+  const readStatus = document.querySelector("input[name='readStatus']:checked").value;
+  const isRead = readStatus === "true";
+  const book = new Book(title, author, pages, isRead);
+  addBookToLibrary(book);
+  dialog.close();
+})
 
 const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 310, true);
 const prophetSong = new Book("Prophet Song", "Paul Lynch", 309, false);
@@ -128,6 +145,6 @@ const itTheClown = new Book("It", "Stephen King", 1184, false);
 const lotr = new Book("Lord of the Rings: The Fellowship of the Ring", "J.R.R. Tolkien", 423, true);
 const got = new Book("A Game Of Thrones", "George R. R. Martin", 694, true);
 
-myLibrary.push(hobbit, prophetSong, itTheClown, lotr, got);
+myLibrary.addBook(hobbit, prophetSong, itTheClown, lotr, got);
 
-displayLibrary(myLibrary);
+displayLibrary();
